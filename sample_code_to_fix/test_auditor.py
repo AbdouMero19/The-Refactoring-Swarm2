f07cb74e1e8fea2544c0835be67923f1b0c795a0
wassim <nw_belguenbour@esi.dz>
2026-01-06 15:36:58 +0000

    new update

--- sample_code_to_fix/test_auditor.py
+++ sample_code_to_fix/test_auditor.py
@@ -6,6 +6,9 @@ def CalculateSum(a,b): # Wrong naming convention (Pylint Convention)
     """
     res = a + b
     print(result) # ERROR: 'result' is not defined (Pylint Error)
-    return res
+    print(wassim) # WARNING: 'wassim' is not defined (Pylint Warning)
+    z=z/0 # ERROR: Division by zero (Pylint Error)
+    
+    return res,z
 
 # Missing final newline (Pylint Convention)
